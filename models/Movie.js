bd74e99e110b5c7fddf07647ed9c55db09b9a484
Phillip Chang <phillip-chang@live.com>
2019-07-21 19:38:32 -0700

    Add movie schema and create mongoose model

---
+++ models/Movie.js
@@ -0,0 +1,28 @@
+var mongoose = require ("mongoose");
+
+// Schema constructor
+var Schema = mongoose.Schema;
+
+var MovieSchema = new Schema({
+
+    title: {
+        type: String,
+        required: true
+    },
+
+    link: {
+        type: String,
+        required: true
+    },
+
+    note: {
+        type: Schema.Types.ObjectId,
+        ref: "Note"
+    }
+});
+
+// Creates model in mongoose
+var Movie = mongoose.model("Movie", MovieSchema);
+
+// Export the Article Model
+module.exports = Article;
