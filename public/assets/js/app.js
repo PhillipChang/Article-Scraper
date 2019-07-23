39dace4c0fbd2eb585e9f558cfaaab944b15f9e2
Phillip Chang <phillip-chang@live.com>
2019-07-22 22:24:41 -0700

    Add modal to cards

--- public/assets/js/app.js
+++ public/assets/js/app.js
@@ -1,29 +1,27 @@
 // Grab the articles as a json
 $.getJSON("/articles", function(data) {
     console.log("client" , data);
-    // for (var i = 0; i < data.length; i++){
-    //     $("#articles").append("<p data-id='"+data[i]._id + "'>" + data[i].title + "<br />" + data[i].link + "</p>");
-    // }
+    for (var i = 0; i < data.length; i++){
+        $("#articles").append("<p data-id='"+data[i]._id + "'>" + data[i].title + "<br />" + data[i].link + "</p>");
+    }
 });
 
 //Create Notes function
-$(document).on("click", "p", function(){
-    $("#notes").empty();
+$(document).ready(function(){
+$(document).on("click", "#add-note", function(){
     var thisId = $(this).attr("data-id");
-
     $.ajax({
         method:"GET",
         url: "/articles/" + thisId
     })
     .then(function(data) {
-        console.log(data);
-        $("#notes").append("<h2>" + data.title + "</h2>");
-         // An input to enter a new title
-      $("#notes").append("<input id='titleinput' name='title' >");
+      console.log(data);
+      $(".modal-content").append("<h6> Would you like to add a Note to: " + data.title + " ?</h6>");
+      // An input to enter a new title
+      $(".modal-content").append("<input id='titleinput' name='title' >");
       // A textarea to add a new note body
-      $("#notes").append("<textarea id='bodyinput' name='body'></textarea>");
+      $(".modal-content").append("<textarea id='bodyinput' name='body'></textarea>");
       // A button to submit a new note, with the id of the article saved to it
-      $("#notes").append("<button data-id='" + data._id + "' id='savenote'>Save Note</button>");
       // If there's a note in the article
       if (data.note) {
         // Place the title of the note in the title input
@@ -33,10 +31,12 @@ $(document).on("click", "p", function(){
       }
     });
 });
+$('.modal').modal();
+});
 
 
 // To save notes
-$(document).on("click", "#savenote", function() {
+$(document).on("click", "#saveNote", function() {
     var thisId = $(this).attr("data-id");
   
     // Run a POST request to change the note, using what's entered in the inputs
@@ -49,10 +49,9 @@ $(document).on("click", "#savenote", function() {
       }
     })
       .then(function(data) {
-        $("#notes").empty();
+        $("#modal-content").empty();
       });
   
     $("#titleinput").val("");
     $("#bodyinput").val("");
   });
-  
